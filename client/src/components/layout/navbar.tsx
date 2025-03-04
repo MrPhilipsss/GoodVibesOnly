879da807908b6ef38e3aa5e0af21dcd56014d9e8
Filip Bartusik <fbartusik#gmail.com>
2025-03-04 04:42:50 +0100

    efwfwf

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -57,7 +57,7 @@ export default function Navbar() {
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <a href="#" className="flex items-center">
           <img src="/gvo-logo-navbar.png" alt="GVO Logo" className="h-8" /> {/* Replaced logo */}
-          <span className="ml-2 text-xl font-bold font-toma">Good Vibes Only</span>
+          <span className="ml-2 text-xl font-toma">Good Vibes Only</span>
         </a>
 
         {/* Desktop Navigation */}
