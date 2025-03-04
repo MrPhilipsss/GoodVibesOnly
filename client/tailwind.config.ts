bbd747bce5c68348acf260f7c00efef269a8acc9
Filip Bartusik <fbartusik#gmail.com>
2025-03-04 04:39:08 +0100

    gwrgw

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -84,6 +84,10 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        'toma': ['"Toma Sans"', 'sans-serif'],
+      },
+      }
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
